c759d57c22c73fd1dbdd60fa32c362de809535d4
ParkChan <hydrarkk@naver.com>
2021-11-15 19:55:10 +0900

    [UI/수정] 함수 호출 직관성을 위해 isNotBlank 로 변경

--- app/src/main/kotlin/com/chan/moviesearcher/ui/MainActivity.kt
+++ app/src/main/kotlin/com/chan/moviesearcher/ui/MainActivity.kt
@@ -45,11 +45,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             job?.cancel()
             job = lifecycleScope.launch {
                 val inputText = text.toString()
-                if (inputText.isBlank()) {
-                    viewModel.clearData()
-                }else{
+                if (inputText.isNotBlank()) {
                     delay(INTERVAL_KEYWORD_SEARCH)
                     viewModel.getMovieList(query = inputText)
+                }else{
+                    viewModel.clearData()
                 }
             }
         }
