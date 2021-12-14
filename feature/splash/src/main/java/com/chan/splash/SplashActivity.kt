9d09c481f995f74d1f6ab1e056647262408affbb
ParkChan <hydrarkk@naver.com>
2021-12-14 16:52:41 +0900

    [수정] - 인트로화면 launchWhenCreated 제거

--- feature/splash/src/main/java/com/chan/splash/SplashActivity.kt
+++ feature/splash/src/main/java/com/chan/splash/SplashActivity.kt
@@ -1,15 +1,24 @@
 package com.chan.splash
 
+import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.chan.splash.databinding.ActivitySplashBinding
 import com.chan.ui.BaseActivity
 import com.chan.util.startActivity
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
 
-    init {
-        lifecycleScope.launchWhenCreated {
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
+        Timber.e(">>>> ${exception.message}")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launch(coroutineExceptionHandler) {
             delay(SPLASH_DELAY_TIME)
             startActivity(BuildConfig.MAIN_ACTIVITY)
             finish()
