5376dfbc3ebdebbc317933c3770d82ea95c43d20
ParkChan <hydrarkk@naver.com>
2021-12-14 17:17:32 +0900

    [수정] - Build.gradle 테스트 디펜던시 복원
    jacoco 생성 관련 테스트 필요

--- feature/splash/src/main/java/com/chan/splash/SplashActivity.kt
+++ feature/splash/src/main/java/com/chan/splash/SplashActivity.kt
@@ -5,27 +5,21 @@ import androidx.lifecycle.lifecycleScope
 import com.chan.splash.databinding.ActivitySplashBinding
 import com.chan.ui.BaseActivity
 import com.chan.util.startActivity
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
 
-    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, exception ->
-        Timber.e(">>>> ${exception.message}")
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch(coroutineExceptionHandler) {
+        lifecycleScope.launch {
             delay(SPLASH_DELAY_TIME)
             startActivity(BuildConfig.MAIN_ACTIVITY)
             finish()
         }
     }
 
-    companion object{
+    companion object {
         private const val SPLASH_DELAY_TIME = 2_000L
     }
 }
